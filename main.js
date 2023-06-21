90147a5a149add0eb3a8adce9f3127531e6cab27
Anannya Sharma <Anannyasharma8300@gmail.com>
2023-06-21 13:15:29 +0530

    end of FAQs

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-// chnage navbar styles on scroll
+// change navbar styles on scroll
 
 window.addEventListener('scroll', ()=>{
     document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0 )
-})
+});
+
+
+// show hide the faq answers
+
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq =>{
+    faq.addEventListener('click', ()=>{
+        faq.classList.toggle('open');
+
+        // change icon
+
+        const icon= faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus' ){
+            icon.className = 'uil uil-minus'
+        }
+        else{
+            icon.className = 'uil uil-plus'
+        }
+    })
+});
